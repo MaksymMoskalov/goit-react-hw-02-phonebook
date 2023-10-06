2c71304bdf6b710021af931ddf611ebe800786ed
Maksim <max.godnavi133@gmail.com>
2023-10-06 04:34:32 +0300

    Home 3 local added

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import css from './Filter.module.css';
 export const Filter = ({ value, onChange }) => {
   return (
-    <label htmlFor="">
+    <label htmlFor="filter">
       <span className={css.input_names}>Find contacts by name</span>
       <input type="text" value={value} onChange={onChange} />
     </label>
